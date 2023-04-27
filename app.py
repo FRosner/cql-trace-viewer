82caf3aadcabe87c73f22e88b3c8bf09876633fc
Frank Rosner <frank.rosner@datastax.com>
2023-04-27 18:33:59 +0200

    first gantt version

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly.express as px
+import datetime
 from dash import Dash, dcc, html, dash_table
 from dash.dependencies import Input, Output
 import dash_cytoscape
@@ -6,6 +8,8 @@ from io import StringIO
 import re
 
 app = Dash(__name__)
+
+trace_gantt = dcc.Graph(id='trace_gantt')
 
 with open('trace.txt', 'r') as trace_file:
     trace_input = dcc.Textarea(
@@ -53,25 +57,14 @@ network = dash_cytoscape.Cytoscape(
 app.layout = html.Div([
     trace_input,
     trace_table,
+    trace_gantt,
     network
 ])
 
 def scale_arrow_width(max_size, size):
     return str(size / max_size * 10 + 1) + "px"
 
-@app.callback(
-    Output(trace_table, 'data'),
-    Output(trace_table, 'columns'),
-    Output(network, 'elements'),
-    Input(trace_input, 'value'),
-    Input(trace_table, 'active_cell')
-)
-def parse_trace(raw_trace, active_cell):
-    df = pd.read_csv(StringIO(raw_trace), sep='\s*\|\s*', header=0, skiprows=[1], engine='python')
-
-    table_data = df.to_dict('records')
-    table_header = [{"name": i, "id": i} for i in df.columns]
-
+def build_network_data(df, active_cell):
     network_df = df
     network_nodes = list(map(lambda n: {'data': {'id': n, 'label': n}}, network_df['source'].unique()))
     network_edges = []
@@ -92,8 +85,55 @@ def parse_trace(raw_trace, active_cell):
 
     network_edges = list(filter(lambda e: (not active_cell) or e['data']['index'] <= active_cell['row'], network_edges))
 
-    network_data = network_nodes + network_edges
-    return table_data, table_header, network_data
+    return network_nodes + network_edges
+
+# TODO add source to label
+# TODO deal with final activity per source
+def build_gantt_fig(df, active_cell):
+    source_root_timestamps = {}
+    gantt_activities = {}
+    for index, row in df.iterrows():
+        activity_timestamp = datetime.datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+        activity_elapsed_timestamp = activity_timestamp + datetime.timedelta(microseconds=int(row['source_elapsed']))
+        if row['source'] not in source_root_timestamps:
+            source_root_timestamps[row['source']] = activity_timestamp
+        if row['source'] not in gantt_activities:
+            gantt_activities[row['source']] = []
+        last_activity = gantt_activities[row['source']][-1:]
+        if last_activity:
+            last_activity[0].update({'end': activity_elapsed_timestamp})
+        gantt_activity = {'activity': row['activity'], 'start': activity_elapsed_timestamp, 'source': row['source']}
+        gantt_activities[row['source']].append(gantt_activity)
+
+    flattened_activities = [item for sublist in list(gantt_activities.values()) for item in sublist]
+    fig_df = pd.DataFrame.from_dict(flattened_activities)
+    gantt_fig = px.timeline(data_frame=fig_df, x_start="start", x_end="end", y="activity")
+    gantt_fig.update_yaxes(autorange="reversed")
+    gantt_fig.update_layout(transition_duration=500)
+    return gantt_fig
+
+@app.callback(
+    Output(trace_table, 'data'),
+    Output(trace_table, 'columns'),
+    Output(network, 'elements'),
+    Output(trace_gantt, 'figure'),
+    Input(trace_input, 'value'),
+    Input(trace_table, 'active_cell')
+)
+def parse_trace(raw_trace, active_cell):
+    try:
+        df = pd.read_csv(StringIO(raw_trace), sep='\s*\|\s*', header=0, skiprows=[1], engine='python')
+
+        table_data = df.to_dict('records')
+        table_header = [{"name": i, "id": i} for i in df.columns]
+
+        network_data = build_network_data(df, active_cell)
+
+        gantt_fig = build_gantt_fig(df, active_cell)
+
+        return table_data, table_header, network_data, gantt_fig
+    except Exception as ex:
+        print(ex)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
