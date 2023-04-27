89205eb988d560d705b088b93d2bfce0f1122867
Frank Rosner <frank.rosner@datastax.com>
2023-04-27 16:59:02 +0200

    step through

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ with open('trace.txt', 'r') as trace_file:
         style={'width': '100%', 'height': 300},
     )
 
-trace_table = dash_table.DataTable(data=[{}])
+trace_table = dash_table.DataTable(
+    data=[{}]
+)
 
 network = dash_cytoscape.Cytoscape(
     id='cytoscape',
@@ -61,18 +63,20 @@ def scale_arrow_width(max_size, size):
     Output(trace_table, 'data'),
     Output(trace_table, 'columns'),
     Output(network, 'elements'),
-    Input(trace_input, 'value')
+    Input(trace_input, 'value'),
+    Input(trace_table, 'active_cell')
 )
-def parse_trace(raw_trace):
+def parse_trace(raw_trace, active_cell):
     df = pd.read_csv(StringIO(raw_trace), sep='\s*\|\s*', header=0, skiprows=[1], engine='python')
 
     table_data = df.to_dict('records')
     table_header = [{"name": i, "id": i} for i in df.columns]
 
-    network_nodes = list(map(lambda n: {'data': {'id': n, 'label': n}}, df['source'].unique()))
+    network_df = df
+    network_nodes = list(map(lambda n: {'data': {'id': n, 'label': n}}, network_df['source'].unique()))
     network_edges = []
     max_message_size = 0
-    for index, row in df.iterrows():
+    for index, row in network_df.iterrows():
         sending_search = re.search('Sending (.*) message to /(.*), size=(.*) bytes', row['activity'], re.IGNORECASE)
 
         if sending_search:
@@ -81,10 +85,12 @@ def parse_trace(raw_trace):
             message_target = sending_search.group(2)
             message_size = sending_search.group(3)
             max_message_size = max(int(message_size), max_message_size)
-            network_edges.append({'data': {'source': message_source, 'target': message_target, 'type': message_type, 'size': message_size}, 'classes': 'message_sent'})
+            network_edges.append({'data': {'source': message_source, 'target': message_target, 'type': message_type, 'size': message_size, 'index': index}, 'classes': 'message_sent'})
 
     for edge in network_edges:
         edge['data'].update({'width': scale_arrow_width(max_message_size, int(edge['data']['size']))})
+
+    network_edges = list(filter(lambda e: (not active_cell) or e['data']['index'] <= active_cell['row'], network_edges))
 
     network_data = network_nodes + network_edges
     return table_data, table_header, network_data
