4546e67de0005029745dfe226464a8870d0bdc27
Frank Rosner <frank.rosner@datastax.com>
2023-06-05 16:26:26 +0200

    remove unused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import datetime
 from dash import Dash, dcc, html, dash_table
 from dash.dependencies import Input, Output
