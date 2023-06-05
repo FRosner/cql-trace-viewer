8e823bf3fd1334c312f115c2094d0c36d600ea79
Frank Rosner <frank.rosner@datastax.com>
2023-06-05 13:30:05 +0200

    plot message graph in scatter plot

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import datetime
 from dash import Dash, dcc, html, dash_table
 from dash.dependencies import Input, Output
-import dash_cytoscape
 from io import StringIO
 import re
 
@@ -24,90 +24,70 @@ trace_table = dash_table.DataTable(
     data=[{}]
 )
 
-network = dash_cytoscape.Cytoscape(
-    id='cytoscape',
-    elements=[],
-    layout={'name': 'breadthfirst'},
-    style={'width': '100%', 'height': '400px'},
-    stylesheet=[
-        {
-            'selector': 'node',
-            'style': {
-                'label': 'data(id)'
-            }
-        },
-        {
-            'selector': 'edge',
-            'style': {
-                # The default curve style does not work with certain arrows
-                'curve-style': 'bezier'
-            }
-        },
-        {
-            'selector': '.message_sent',
-            'style': {
-                'target-arrow-shape': 'triangle',
-                'label': 'data(type)',
-                'width': 'data(width)'
-            }
-        }
-    ],
-    userZoomingEnabled=False,
-    userPanningEnabled=False
-)
-
 app.layout = html.Div([
     trace_input,
     trace_gantt,
-    trace_table,
-    network
+    trace_table
 ])
 
 def scale_arrow_width(max_size, size):
     return str(size / max_size * 10 + 1) + "px"
 
-def build_network_data(df, active_cell):
-    network_df = df
-    network_nodes = list(map(lambda n: {'data': {'id': n, 'label': n}}, network_df['source'].unique()))
-    network_edges = []
-    max_message_size = 0
-    for index, row in network_df.iterrows():
-        sending_search = re.search('Sending (.*) message to /(.*), size=(.*) bytes', row['activity'], re.IGNORECASE)
 
+def build_scatter_fig(df):
+    source_root_timestamps = {}
+    trace_activities = {}
+    sent_messages = []
+    messages = []
+    for index, row in df.iterrows():
+        # Build activities
+        source = row['source']
+        activity_timestamp = datetime.datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+        if source not in source_root_timestamps:
+            source_root_timestamps[source] = activity_timestamp
+        if source not in trace_activities:
+            trace_activities[source] = []
+
+        activity_elapsed_timestamp = source_root_timestamps[source] + datetime.timedelta(microseconds=int(row['source_elapsed']))
+
+        gantt_activity = {'activity': row['activity'], 'timestamp': activity_timestamp, 'start': activity_elapsed_timestamp, 'source': source}
+        trace_activities[source].append(gantt_activity)
+
+        # Collect messages being sent
+        sending_search = re.search('Sending (.*) message to /(.*), size=(.*) bytes', row['activity'], re.IGNORECASE)
         if sending_search:
             message_source = row['source']
             message_type = sending_search.group(1)
             message_target = sending_search.group(2)
             message_size = sending_search.group(3)
-            max_message_size = max(int(message_size), max_message_size)
-            network_edges.append({'data': {'source': message_source, 'target': message_target, 'type': message_type, 'size': message_size, 'index': index}, 'classes': 'message_sent'})
+            sent_messages.append({'source': message_source, 'target': message_target, 'type': message_type, 'size': message_size,
+                                  'source_activity': row['activity'], 'source_start': activity_elapsed_timestamp})
 
-    for edge in network_edges:
-        edge['data'].update({'width': scale_arrow_width(max_message_size, int(edge['data']['size']))})
+        # Match received messages with sent messages
+        receiving_search = re.search('(.*) message received from /(.*) ', row['activity'], re.IGNORECASE)
+        if receiving_search:
+            message_target = row['source']
+            message_type = receiving_search.group(1)
+            message_source = receiving_search.group(2)
+            corresponding_sent_messages = [sent_message for sent_message in sent_messages if
+                                           sent_message['source'] == message_source
+                                           and sent_message['type'] == message_type
+                                           and sent_message['target'] == message_target]
+            if corresponding_sent_messages:
+                corresponding_sent_message = corresponding_sent_messages[0]
+                sent_messages.remove(corresponding_sent_message)
+                messages.append({**corresponding_sent_message, 'target_activity': row['activity'], 'target_start': activity_elapsed_timestamp})
 
-    network_edges = list(filter(lambda e: (not active_cell) or e['data']['index'] <= active_cell['row'], network_edges))
-
-    return network_nodes + network_edges
-
-def build_scatter_fig(df):
-    source_root_timestamps = {}
-    gantt_activities = {}
-    for index, row in df.iterrows():
-        source = row['source']
-        activity_timestamp = datetime.datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
-        if source not in source_root_timestamps:
-            source_root_timestamps[source] = activity_timestamp
-        if source not in gantt_activities:
-            gantt_activities[source] = []
-
-        activity_elapsed_timestamp = source_root_timestamps[source] + datetime.timedelta(microseconds=int(row['source_elapsed']))
-
-        gantt_activity = {'activity': row['activity'], 'timestamp': activity_timestamp, 'start': activity_elapsed_timestamp, 'source': source}
-        gantt_activities[source].append(gantt_activity)
-
-    flattened_activities = [item for sublist in list(gantt_activities.values()) for item in sublist]
+    flattened_activities = [item for sublist in list(trace_activities.values()) for item in sublist]
     fig_df = pd.DataFrame.from_records(flattened_activities)
     fig = px.scatter(data_frame=fig_df, x='start', y='activity', color='source')
+
+    for message in messages:
+        fig.add_trace(go.Scatter(
+            x=[message["source_start"], message["target_start"]],
+            y=[message['source_activity'], message["target_activity"]],
+            mode='lines', name='now', showlegend=False, line=go.scatter.Line(color="gray", dash="dash"),
+        ))
     fig.update_yaxes(autorange="reversed")
     fig.update_layout(transition_duration=500)
     return fig
@@ -115,26 +95,21 @@ def build_scatter_fig(df):
 @app.callback(
     Output(trace_table, 'data'),
     Output(trace_table, 'columns'),
-    Output(network, 'elements'),
     Output(trace_gantt, 'figure'),
     Output(trace_gantt, 'style'),
     Input(trace_input, 'value'),
-    Input(trace_table, 'active_cell')
 )
-def parse_trace(raw_trace, active_cell):
+def parse_trace(raw_trace):
     try:
         if raw_trace:
             df = pd.read_csv(StringIO(raw_trace), sep='\s*\|\s*', header=0, skiprows=[1], engine='python')
 
             table_data = df.to_dict('records')
             table_header = [{"name": i, "id": i} for i in df.columns]
-
-            network_data = build_network_data(df, active_cell)
-
             scatter_fig = build_scatter_fig(df)
             scatter_style = {'width': '100%', 'height': str(len(df) * 40) + 'px'}
 
-            return table_data, table_header, network_data, scatter_fig, scatter_style
+            return table_data, table_header, scatter_fig, scatter_style
         else:
             return [], [], [], {}, {}
     except Exception as ex:
